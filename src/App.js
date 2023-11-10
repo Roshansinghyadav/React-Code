249eac3c5b3f331355d9da77e8c95829ef00a7f5
Roshan Singh Yadav <callmersy8031@gmail.com>
2023-11-10 13:54:40 +0530

    JSX in react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a = 5
+  return (
+    <>
+    <h1>Roshan singh yadav</h1>
+    <h2>{2+3}</h2>
+    <h3>{(a)>10?"greater":"Smaller"}</h3>
+    </>
+  );
+}
+
+export default App;
